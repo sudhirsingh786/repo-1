2fdd54e713f1165494ac5b33a51208e4391861ab
sudhirsingh786 <123869147+sudhirsingh786@users.noreply.github.com>
2026-01-29 13:14:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,7 @@ module "service_account" {
   sa_name    = var.sa_name
   labels     = var.labels
 }
-output "service_account_email" {
-  value = google_service_account.this.email
-}
+
 output "state_change_marker" {
   value = timestamp()
 }
